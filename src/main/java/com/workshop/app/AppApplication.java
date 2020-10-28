eb932695c41062441c9c61b5f75e6edf93dcebf4
Oracle Public Cloud User <opc@bastion.lbsubce1e758d4.cluster1.oraclevcn.com>
2020-10-28 23:13:18 +0000

    first commit

---
+++ src/main/java/com/workshop/app/AppApplication.java
@@ -0,0 +1,27 @@
+package com.workshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+public class AppApplication {
+
+	@RequestMapping("/", method = GET)
+	@ResponseBody
+	public String home() {
+		String mensagem = "Ola Mundo";
+		String m = mensagem;
+		return m;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
